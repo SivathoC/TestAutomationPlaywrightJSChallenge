421a3972e327094c2199326ac21c95b4e80deb57
SivathoC <sivathoc@gmail.com>
2025-07-18 02:18:14 +0200

    Modified testdataUtils file - introducing getInventoryDetails method Added Inventory Details model

--- src/main/utils/testdataUtil.js
+++ src/main/utils/testdataUtil.js
@@ -1,4 +1,5 @@
 const UserCredential = require('../../test/models/UserCredential.js');
+const InventoryDetails = require('../../test/models/InventoryDetails.js');
 
 async function getLoginCredentials(page) {
   const usernameText = await page.locator('#login_credentials').innerText();
@@ -20,4 +21,20 @@ async function getLoginCredentials(page) {
 
   return credentials;
 }   
+module.exports = { getLoginCredentials };
+
+async function getInventoryDetails(page) {
+  const items = await page.$$('[data-test="inventory-item"]');
+  const inventoryDetails = [];
+
+  for (const item of items) {
+    const title = await item.locator('.inventory_item_name').textContent();
+    const price = await item.locator('.inventory_item_price').textContent();
+    const description = await item.locator('.inventory_item_desc').textContent();
+    const imgLocator = page.locator('img[data-test="inventory-item-sauce-labs-backpack-img"]');
+    const imageSrc = await imgLocator.getAttribute('src');
+    inventoryDetails.push(new InventoryDetails(title, price, description, imageSrc));
+  }
+  return inventoryDetails;
+}
 module.exports = { getLoginCredentials };
